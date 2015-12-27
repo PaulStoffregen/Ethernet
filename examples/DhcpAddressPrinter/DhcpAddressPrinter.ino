cbb8686ecbb342d31fc65aaae858c36c1a76a7ca
PaulStoffregen <paul@pjrc.com>
2015-12-26 18:46:12 -0800

    Update DhcpAddressPrinter example

--- examples/DhcpAddressPrinter/DhcpAddressPrinter.ino
+++ examples/DhcpAddressPrinter/DhcpAddressPrinter.ino
@@ -1,16 +1,18 @@
 /*
   DHCP-based IP printer
 
- This sketch uses the DHCP extensions to the Ethernet library
- to get an IP address via DHCP and print the address obtained.
- using an Arduino Wiznet Ethernet shield.
+  This sketch uses the DHCP extensions to the Ethernet library
+  to get an IP address via DHCP and print the address obtained.
+  using an Arduino Wiznet Ethernet shield.
 
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
+  Circuit:
+   Ethernet shield attached to pins 10, 11, 12, 13
 
- created 12 April 2011
- modified 9 Apr 2012
- by Tom Igoe
+  created 12 April 2011
+  modified 9 Apr 2012
+  by Tom Igoe
+  modified 02 Sept 2015
+  by Arturo Guadalupi
 
  */
 
@@ -20,12 +22,8 @@
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = {
-  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -36,6 +34,7 @@ void setup() {
   }
 
   // start the Ethernet connection:
+  Serial.println("Initialize Ethernet with DHCP:");
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
@@ -43,17 +42,42 @@ void setup() {
       ;
   }
   // print your local IP address:
-  Serial.print("My IP address: ");
-  for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    // print the value of each byte of the IP address:
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print(".");
-  }
-  Serial.println();
+  printIPAddress();
 }
 
 void loop() {
+  switch (Ethernet.maintain()) {
+    case 1:
+      //renewed fail
+      Serial.println("Error: renewed fail");
+      break;
 
+    case 2:
+      //renewed success
+      Serial.println("Renewed success");
+      //print your local IP address:
+      printIPAddress();
+      break;
+
+    case 3:
+      //rebind fail
+      Serial.println("Error: rebind fail");
+      break;
+
+    case 4:
+      //rebind success
+      Serial.println("Rebind success");
+      //print your local IP address:
+      printIPAddress();
+      break;
+
+    default:
+      //nothing happened
+      break;
+  }
 }
 
-
+void printIPAddress() {
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+}
