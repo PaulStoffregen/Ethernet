7c452a96bb4c186239d28754cef40d86a5a973f9
PaulStoffregen <paul@pjrc.com>
2017-10-26 06:59:02 -0700

    Improve EthernetServer handling of unexpected remote disconnect

--- Ethernet.h
+++ Ethernet.h
@@ -180,15 +180,14 @@ private:
 class EthernetServer : public Server {
 private:
 	uint16_t _port;
-	uint8_t sockindex;
-	void accept();
 public:
-	EthernetServer(uint16_t port) : _port(port), sockindex(MAX_SOCK_NUM) { }
+	EthernetServer(uint16_t port) : _port(port) { }
 	EthernetClient available();
 	virtual void begin();
 	virtual size_t write(uint8_t);
 	virtual size_t write(const uint8_t *buf, size_t size);
 	using Print::write;
+	//void statusreport();
 
 	// TODO: make private when socket allocation moves to EthernetClass
 	static uint16_t server_port[MAX_SOCK_NUM];
